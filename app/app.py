cdb1e2b90b3a8574a62b51f8f56bbe6a2c4739f3
Elias Mendez <elivanlender@gmail.com>
2022-12-07 15:37:12 -0600

    botones

--- app/app.py
+++ app/app.py
@@ -77,7 +77,6 @@ def boton1():
     return render_template('index.html', DATO="Signal 1: Activated")
 
 @app.route("/boton2", methods=['POST'])
-manual=0
 def boton2():
     GPIO.cleanup()
     GPIO.output(Out_1,GPIO.LOW)

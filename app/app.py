ebe0cbf80e5791acb2da5dbffc4b4b3bbdff2d5a
Elias Mendez <elivanlender@gmail.com>
2022-12-06 18:02:32 -0600

    Activate-Deactivate

--- app/app.py
+++ app/app.py
@@ -69,19 +69,19 @@ def index():
     GPIO.cleanup()
     GPIO.output(Out_9, GPIO.LOW)
     GPIO.output(Out_1, GPIO.LOW)
-    return render_template('index.html', DATO="La señal 1 está desactivada")
+    return render_template('index.html', DATO="Signal 1: Deactivate")
 
 @app.route("/boton1", methods=['POST'])
 def boton1():
     GPIO.cleanup()
     GPIO.output(Out_1,GPIO.HIGH)
-    return render_template('index.html', DATO="La señal 1 está Encendida")
+    return render_template('index.html', DATO="Signal 1: Activate")
 
 @app.route("/boton2", methods=['POST'])
 def boton2():
     GPIO.cleanup()
     GPIO.output(Out_1,GPIO.LOW)
-    return render_template('index.html', DATO="La señal 1 está desactivada")
+    return render_template('index.html', DATO="Signal 1: Deactivate")
 
 if __name__=='__main__':
     app.run(host='192.168.100.232', port=5500)
